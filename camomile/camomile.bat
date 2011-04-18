3004f9d214c4d0969c88dfe2385fae1e6c8cc2bd
William Wragg <wm.wragg@gmail.com>
2011-04-18 18:22:36 +0100

    Updated the Windows .bat file to work correctly

--- camomile/camomile.bat
+++ camomile/camomile.bat
@@ -18,7 +18,7 @@ rem ###########################################################################
 cd bin
 call awk95 -v OS="WINDOWS" -f json.awk -f connections.awk
 
-set JETTY_PORT="-Djetty.port=%1"
+set JETTY_PORT=-Djetty.port=%1
 if not "%JETTY_PORT%" == "-Djetty.port=" goto gotPort
 set JETTY_PORT=""
 :gotPort
